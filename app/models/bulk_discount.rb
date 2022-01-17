b0cee0bc366c50b5155efe48751afa0f58c1e08a
Joel Grant <joelmgrant@me.com>
2022-01-17 14:01:37 -0700

    Create the Bi-directional relationship with the new items association

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,5 +2,11 @@ class BulkDiscount < ApplicationRecord
   validates_presence_of :discount, :threshold
   validates_numericality_of :discount, allow_nil: false
   validates_numericality_of :threshold, allow_nil: false
+
   belongs_to :merchant
+  has_many :items, through: :merchant
+
+  
+
+
 end
